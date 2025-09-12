e342aa10436ec75a0f91454c6df31723d77e860e
harshitNextCrest <neeu.singh327@gmail.com>
2025-09-13 01:35:29 +0530

    final_commit_2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
@@ -6,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    log: ["query", "error", "warn"], // optional for debugging
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
